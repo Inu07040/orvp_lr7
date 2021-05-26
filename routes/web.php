df30560a54aab354624e79ba091ef9e5d4fbd968
Juliya Ipatova <inu07040@mail.ru>
2021-05-27 00:17:14 +0300

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('cart', [ProductController::class, 'cart']);
-Route::get('add-to-cart/{id}', [ProductController::class, 'addToCart']);
+Route::patch('add-to-cart/{id}', [ProductController::class, 'addToCart']);
 
 Route::patch('update-cart', [ProductController::class, 'updateCart']);
 Route::delete('remove-from-cart', [ProductController::class, 'removeCart']);
 
 Route::resource('products', ProductController::class);
+Route::get('products', [ProductController::class, 'index']);
